7331b12cb02b7be674fe5ba26b29fe25858c0669
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-03-16 17:55:12 -0700

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-  $('.slider').slick({ 
-  	infinite: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	variableWidth: false,
-	dots: true,
-	arrows: true
+	console.log("READY");
+	$('.slider').slick({ 
+		// infinite: true,
+		// slidesToShow: 1,
+		// slidesToScroll: 1,
+		// variableWidth: false,
+		// dots: true,
+		// arrows: true,
+		autoplay: true
+	});
 });
